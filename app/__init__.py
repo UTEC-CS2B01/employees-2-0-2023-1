c281bd2f35ec621840b2f32d382737b284a5ff58
FernandoUs <fernando.usurin@utec.edu.pe>
2023-05-24 18:44:48 -0500

    Update to Departaments

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask import (
 from models import db, setup_db, Employee, Department
 from flask_cors import CORS
 from utilities import allowed_file
+from datetime import datetime
+
 
 import os
 import sys
@@ -24,90 +26,93 @@ def create_app(test_config=None):
         return response
     
 
-    @app.route('/employees', methods=['POST'])
+    @app.route('/employees', methods=['GET','POST', 'PATCH', 'DELETE'])
     def create_employee():
-        returned_code = 200
-        list_errors = []
-        try:
-            body = request.form
+        if request.method == 'GET':
+            employees = Employee.query.filter_by(is_active=True).all()
+            employees = [employee.serialize() for employee in employees]
+            return jsonify({'success': True, 'employees': employees}), 200
+        
+        if request.method == 'POST':
+            returned_code = 200
+            list_errors = []
+            try:
+                body = request.form
 
-            if 'firstname' not in body:
-                list_errors.append('firstname is required')
-            else:
-                firstname = request.form.get('firstname')
+                if 'firstname' not in body:
+                    list_errors.append('firstname is required')
+                else:
+                    firstname = request.form.get('firstname')
 
-            if 'lastname' not in body:
-                list_errors.append('lastname is required')    
-            else:
-                lastname = request.form['lastname']
+                if 'lastname' not in body:
+                    list_errors.append('lastname is required')    
+                else:
+                    lastname = request.form['lastname']
 
-            if 'age' not in body:
-                list_errors.append('age is required')    
-            else:
-                age = request.form['age']
+                if 'age' not in body:
+                    list_errors.append('age is required')    
+                else:
+                    age = request.form['age']
 
-            if 'selectDepartment' not in body:
-                list_errors.append('selectDepartment is required')
-            else:
-                department_id = request.form['selectDepartment']
+                if 'selectDepartment' not in body:
+                    list_errors.append('selectDepartment is required')
+                else:
+                    department_id = request.form['selectDepartment']
 
-            if 'image' not in request.files:
-                list_errors.append('image is required')
-            else:
                 if 'image' not in request.files:
-                    return jsonify({'success': False, 'message': 'No image provided by the employee'}), 400
-        
-                file = request.files['image']
+                    list_errors.append('image is required')
+                else:
+                    if 'image' not in request.files:
+                        return jsonify({'success': False, 'message': 'No image provided by the employee'}), 400
+            
+                    file = request.files['image']
 
-                if file.filename == '':
-                    return jsonify({'success': False, 'message': 'No image selected'}), 400
-        
-                if not allowed_file(file.filename):
-                    return jsonify({'success': False, 'message': 'Image format not allowed'}), 400
-        
+                    if file.filename == '':
+                        return jsonify({'success': False, 'message': 'No image selected'}), 400
+            
+                    if not allowed_file(file.filename):
+                        return jsonify({'success': False, 'message': 'Image format not allowed'}), 400
+            
 
-            if len(list_errors) > 0:
-                returned_code = 400
+                if len(list_errors) > 0:
+                    returned_code = 400
+                else:
+                    employee = Employee(firstname, lastname, age, department_id)
+                    db.session.add(employee)
+                    db.session.commit()
+
+                    employee_id = employee.id
+
+                    cwd = os.getcwd()
+
+                    employee_dir = os.path.join(app.config['UPLOAD_FOLDER'], employee.id)
+                    os.makedirs(employee_dir, exist_ok=True)
+
+                    upload_folder = os.path.join(cwd, employee_dir)
+
+                    file.save(os.path.join(upload_folder, file.filename))
+
+                    employee.image = file.filename
+                    db.session.commit()
+
+            except Exception as e:
+                print(e)
+                print(sys.exc_info())
+                db.session.rollback()
+                returned_code = 500
+
+            finally:
+                db.session.close()
+
+            if returned_code == 400:
+                return jsonify({'success': False, 'message': 'Error creating employee', 'errors': list_errors}), returned_code
+            elif returned_code == 500:
+                return jsonify({'success': False, 'message': 'Error creating employee'}), returned_code
             else:
-                employee = Employee(firstname, lastname, age, department_id)
-                db.session.add(employee)
-                db.session.commit()
-
-                employee_id = employee.id
-
-                cwd = os.getcwd()
-
-                employee_dir = os.path.join(app.config['UPLOAD_FOLDER'], employee.id)
-                os.makedirs(employee_dir, exist_ok=True)
-
-                upload_folder = os.path.join(cwd, employee_dir)
-
-                file.save(os.path.join(upload_folder, file.filename))
-
-                employee.image = file.filename
-                db.session.commit()
-
-        except Exception as e:
-            print(e)
-            print(sys.exc_info())
-            db.session.rollback()
-            returned_code = 500
-
-        finally:
-            db.session.close()
-
-        if returned_code == 400:
-            return jsonify({'success': False, 'message': 'Error creating employee', 'errors': list_errors}), returned_code
-        elif returned_code == 500:
-            return jsonify({'success': False, 'message': 'Error creating employee'}), returned_code
-        else:
-            return jsonify({'id': employee_id, 'success': True, 'message': 'Employee Created successfully!'}), returned_code
+                return jsonify({'id': employee_id, 'success': True, 'message': 'Employee Created successfully!'}), returned_code
     
-    @app.route('/departaments', methods=['GET','POST','PATCH','DELETE'])
+    @app.route('/departaments', methods=['GET', 'POST', 'PATCH', 'DELETE'])
     def create_departament():
-
-        returned_code = 200
-        list_errors = []
         if request.method == 'GET':
             try:
                 departaments = Department.query.all()
@@ -117,10 +122,14 @@ def create_app(test_config=None):
                 print(e)
                 print(sys.exc_info())
                 db.session.rollback()
-                returned_code = 500
+                return jsonify({'success': False, 'message': 'Error obtener departaments'}), 500
             finally:
                 db.session.close()
+
         if request.method == 'POST':
+            returned_code = 200
+            list_errors = []
+
             try:
                 body = request.form
 
@@ -130,13 +139,14 @@ def create_app(test_config=None):
                     name = request.form.get('name')
 
                 if 'short_name' not in body:
-                    list_errors.append('short name is required')    
+                    list_errors.append('short name is required')
                 else:
-                    shot_name = request.form.get('short_name')
+                    short_name = request.form.get('short_name')
+
                 if len(list_errors) > 0:
                     returned_code = 400
                 else:
-                    departament = Department(name, shot_name)
+                    departament = Department(name, short_name)
                     db.session.add(departament)
                     db.session.commit()
 
@@ -150,30 +160,61 @@ def create_app(test_config=None):
 
             finally:
                 db.session.close()
+
+            if returned_code == 400:
+                return jsonify({'success': False, 'message': 'Error creating departament', 'errors': list_errors}), returned_code
+            elif returned_code == 500:
+                return jsonify({'success': False, 'message': 'Error creating departament'}), returned_code
+            else:
+                return jsonify({'id': departament_id, 'success': True, 'message': 'Departament Created successfully!'}), returned_code
+
         if request.method == 'PATCH':
-        #Vamos a actualizar un departamento parcialmente
             new_name = request.json.get('name')
             new_short_name = request.json.get('short_name')
 
-            # Realiza la actualización de todos los registros
-            departments = Department.query.all()
-            for department in departments:
-                if new_name is not None:
-                    department.name = new_name
-                if new_short_name is not None:
-                    department.short_name = new_short_name
-                department.modified_at = datetime.utcnow()
-            #Cambiamos la fecha de actualización
-                db.session.commit()
-            return jsonify({'message': 'Departments updated successfully'})
+            try:
+                departments = Department.query.all()
+                for department in departments:
+                    if new_name is not None:
+                        department.name = new_name
+                        department.modified_at = datetime.utcnow()
+                    if new_short_name is not None:
+                        department.short_name = new_short_name
+                        department.modified_at = datetime.utcnow()
 
+                db.session.commit()
+
+                return jsonify({'message': 'Departments updated successfully'})
+
+            except Exception as e:
+                print(e)
+                print(sys.exc_info())
+                db.session.rollback()
+                return jsonify({'success': False, 'message': 'Error updating departaments'}), 500
+
+            finally:
+                db.session.close()
+
+        if request.method == 'DELETE':
+            try:
+                departments = Department.query.all()
+                for department in departments:
+                    db.session.delete(department)
+
+                db.session.commit()
+
+                return jsonify({'message': 'Departments deleted successfully'})
+
+            except Exception as e:
+                print(e)
+                print(sys.exc_info())
+                db.session.rollback()
+                return jsonify({'success': False, 'message': 'Error deleting departaments'}), 500
+
+            finally:
+                db.session.close()
+        
             
 
-        if returned_code == 400:
-            return jsonify({'success': False, 'message': 'Error creating departament', 'errors': list_errors}), returned_code
-        elif returned_code == 500:
-            return jsonify({'success': False, 'message': 'Error creating departament'}), returned_code
-        else:
-            return jsonify({'id': departament_id, 'success': True, 'message': 'Departament Created successfully!'}), returned_code
-
+        
     return app
