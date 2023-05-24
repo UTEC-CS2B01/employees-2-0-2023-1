bfce895e4c25fbb9999c0086077fe935eb0b9362
mabisrror <mabisrror@utec.edu.pe>
2023-05-23 19:07:48 -0500

    0005 - handling errors

--- app/__init__.py
+++ app/__init__.py
@@ -26,49 +26,81 @@ def create_app(test_config=None):
 
     @app.route('/employees', methods=['POST'])
     def create_employee():
+        returned_code = 200
+        list_errors = []
         try:
-            firstname = request.form.get('firstname')
-            lastname = request.form['lastname']
-            age = request.form['age']
-            department_id = request.form['selectDepartment']
+            body = request.form
+
+            if 'firstname' not in body:
+                list_errors.append('firstname is required')
+            else:
+                firstname = request.form.get('firstname')
+
+            if 'lastname' not in body:
+                list_errors.append('lastname is required')    
+            else:
+                lastname = request.form['lastname']
+
+            if 'age' not in body:
+                list_errors.append('age is required')    
+            else:
+                age = request.form['age']
+
+            if 'selectDepartment' not in body:
+                list_errors.append('selectDepartment is required')
+            else:
+                department_id = request.form['selectDepartment']
 
             if 'image' not in request.files:
-                return jsonify({'success': False, 'message': 'No image provided by the employee'}), 400
+                list_errors.append('image is required')
+            else:
+                if 'image' not in request.files:
+                    return jsonify({'success': False, 'message': 'No image provided by the employee'}), 400
         
-            file = request.files['image']
+                file = request.files['image']
 
-            if file.filename == '':
-                return jsonify({'success': False, 'message': 'No image selected'}), 400
+                if file.filename == '':
+                    return jsonify({'success': False, 'message': 'No image selected'}), 400
         
-            if not allowed_file(file.filename):
-                return jsonify({'success': False, 'message': 'Image format not allowed'}), 400
+                if not allowed_file(file.filename):
+                    return jsonify({'success': False, 'message': 'Image format not allowed'}), 400
         
 
-            employee = Employee(firstname, lastname, age, department_id)
-            db.session.add(employee)
-            db.session.commit()
+            if len(list_errors) > 0:
+                returned_code = 400
+            else:
+                employee = Employee(firstname, lastname, age, department_id)
+                db.session.add(employee)
+                db.session.commit()
 
-            cwd = os.getcwd()
+                employee_id = employee.id
 
-            employee_dir = os.path.join(app.config['UPLOAD_FOLDER'], employee.id)
-            os.makedirs(employee_dir, exist_ok=True)
+                cwd = os.getcwd()
 
-            upload_folder = os.path.join(cwd, employee_dir)
+                employee_dir = os.path.join(app.config['UPLOAD_FOLDER'], employee.id)
+                os.makedirs(employee_dir, exist_ok=True)
 
-            file.save(os.path.join(upload_folder, file.filename))
+                upload_folder = os.path.join(cwd, employee_dir)
 
-            employee.image = file.filename
-            db.session.commit()
+                file.save(os.path.join(upload_folder, file.filename))
 
-            return jsonify({'id': employee.id, 'success': True, 'message': 'Employee Created successfully!'}), 200
+                employee.image = file.filename
+                db.session.commit()
+
         except Exception as e:
             print(e)
             print(sys.exc_info())
             db.session.rollback()
-            return jsonify({'success': False, 'message': 'Error creating employee'}), 500
+            returned_code = 500
 
         finally:
             db.session.close()
 
+        if returned_code == 400:
+            return jsonify({'success': False, 'message': 'Error creating employee', 'errors': list_errors}), returned_code
+        elif returned_code == 500:
+            return jsonify({'success': False, 'message': 'Error creating employee'}), returned_code
+        else:
+            return jsonify({'id': employee_id, 'success': True, 'message': 'Employee Created successfully!'}), returned_code
 
     return app
--- readme.md
+++ readme.md
@@ -4,6 +4,24 @@
 
 ## Operations
 
+### Create Employee
+
 ```
 curl -F "first_name=Juan" -F "last_name=perez" -F "job_title=Reclutador" -F "selectDepartment=3cf975cb-ad8f-4e79-ac83-a9f4a0f466c9" -F "image=@cristiano.jpeg;type=image/jpeg" -X POST http://localhost:5004/employees
+```
+
+### Errors
+
+```
+{
+  "errors": [
+    "firstname is required",
+    "lastname is required",
+    "age is required",
+    "selectDepartment is required",
+    "image is required"
+  ],
+  "message": "Error creating employee",
+  "success": false
+}
 ```
