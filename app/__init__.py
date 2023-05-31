89da7f9093bfd4fb8e9592852c4d7d64a2fd9278
Josue Velo <josue.velo@utec.edu.pe>
2023-05-31 18:08:12 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -520,5 +520,5 @@ def create_app(test_config=None):
             return jsonify({'success': False, 'message': error_message}), returned_code
 
         return jsonify({'success': True, 'message': 'Employee departments removed successfully!'}), returned_code
-
+# Post a successful
     return app
