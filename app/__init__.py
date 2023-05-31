432415f289a73fe5358a768090e2478dddbdbc8d
Josue Velo <josue.velo@utec.edu.pe>
2023-05-30 22:10:03 -0500

    Hola

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,9 @@ from flask import (
 from .models import db, setup_db, Employee, Department
 from flask_cors import CORS
 from .utilities import allowed_file
-
 import os
 import sys
-
+################################################################
 def create_app(test_config=None):
     app = Flask(__name__)
     with app.app_context():
