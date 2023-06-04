7ab486ccc0c262749a5a526996a348d9687cc939
denzelbautista <denzel.bautista@utec.edu.pe>
2023-06-04 10:15:53 -0500

    mios

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ def create_app(test_config=None):
         response.headers.add(' Access-Control-Max-Age', '10')
         return response
     
-        # POST
+    # POST
     #########################################################
 
     @app.route('/employees', methods=['POST'])
