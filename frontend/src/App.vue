e9d22cce76f3f13348aa1eb379ecca035280863e
marvin4keap <marvin.abisrror@keap.com>
2023-06-13 18:04:23 -0500

    UTEC-0007 remove hello world

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,12 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>The counter is: {{ count }}</p>
+    <button @click="increment">Increment count</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      count: 0,
+      name: "",
+      lastname: "",
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
   },
 };
 </script>
