2467c9e0c8f6cf37c20b7182c285d0d1237ba9de
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2023-06-21 16:57:54 -0500

    Creacion de las paginas

---
+++ frontend/src/components/CreateEmployee.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="employee-creation-form">
+      <h1>Create Employee</h1>
+      <form @submit.prevent.stop="createEmployee">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model.lazy.trim="employee.name" />
+        </div>
+        <div class="form-group">
+          <label for="short-name">Short Name:</label>
+          <input type="text" id="short-name" v-model="employee.shortName" />
+        </div>
+        <button class="submit-button" type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { createEmployee } from "@/services/employees.api";
+  
+  export default {
+    name: "CreateEmployee",
+    data() {
+      return {
+        employee: {
+          name: "",
+          shortName: "",
+        },
+      };
+    },
+    methods: {
+      async createEmployee() {
+        console.log(this.employee);
+        const data = await createEmployee(this.employee);
+        console.log(data);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .employee-creation-form {
+    padding: 0;
+    margin: 0;
+    width: 300px;
+  }
+  
+  .form-group {
+    margin-bottom: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+  
+  input[type="text"] {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    width: 300px;
+  }
+  
+  .submit-button {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  </style>
---
+++ frontend/src/components/ListEmployees.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="employee-list">
+      <h1>List of Employees</h1>
+      <ul>
+        <li v-for="employee in employees" :key="employee.id">
+          {{ employee.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { getAllEmployees } from "@/services/employees.api";
+  
+  export default {
+    name: "EmployeeList",
+    data() {
+      return {
+        employees: [],
+      };
+    },
+    async mounted() {
+      await this.fetchEmployees();
+    },
+    methods: {
+      async fetchEmployees() {
+        const { employees } = await getAllEmployees();
+        this.employees = employees;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .employee-list {
+    padding: 0;
+    margin: 0;
+    width: 300px;
+  }
+  </style>
