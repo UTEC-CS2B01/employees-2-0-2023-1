7323c7c6a072a322351c4e57b932322532d86e19
Fabrizzio <fabrizzio.vilchez.e@utec.edu.pe>
2023-06-27 15:07:17 -0500

    User_controller completed

--- backend/app/users_controller.py
+++ backend/app/users_controller.py
@@ -1,11 +1,15 @@
 from flask import (
     Blueprint,
     request,
+    jsonify,
 )
 
-from .models import User
+from .models import User, db
+
+import sys
 
 users_bp = Blueprint('/users', __name__)
+
 
 @users_bp.route('/users', methods=['POST'])
 def create_user():
@@ -19,7 +23,7 @@ def create_user():
         else:
             username = body.get('username')
 
-        user_db = User.query.filter(User.username==username).first()
+        user_db = User.query.filter(User.username == username).first()
 
         if 'password' not in body:
             error_lists.append('password is required')
@@ -31,15 +35,29 @@ def create_user():
         else:
             confirmationPassword = body.get('confirmationPassword')
 
-
         if user_db is not None:
-            #... 
+            error_lists.append('Username already exists')
 
+        if len(error_lists) == 0:
+            new_user = User(username=username, password=password)
 
-        
-
-
+            db.session.add(new_user)
+            db.session.commit()
 
     except Exception as e:
+        print(sys.exc_info())
+        db.session.rollback()
+        returned_code = 500
 
     finally:
+        db.session.close()
+
+    if len(error_lists) > 0:
+        returned_code = 400
+
+    if returned_code == 201:
+        return jsonify({'success': False, 'user': new_user.serialize}), returned_code
+    elif returned_code == 400:
+        return jsonify({'success': False, 'errors': error_lists}), returned_code
+    else:
+        return jsonify({'success': False, 'message': 'Internal server error'}), returned_code
